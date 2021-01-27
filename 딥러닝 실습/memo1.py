5d56cc2cac4702fbcce5bfca48c255cd6a34c751
Mei <clara_31@naver.com>
2021-01-27 14:10:48 +0900

    210127_DLstudy

--- 딥러닝 실습/memo1.py
+++
@@ -1,4 +0,0 @@
-#### 210126 모두를 위한 딥러닝 강좌 - 기초부터 이론 공부 + 메모장 기록
-### https://www.youtube.com/watch?v=BS6O0zOGX4E&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm&index=1
-## Lec 00 - ML/DL 수업 개요와 일정
-## Lect 01 - 기본적인 ML의 용어와 개념 설정
