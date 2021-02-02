fa43a0b0887636082a312e5f26cd6673dbc4b6aa
hyunjoebrother <hyunjoe31@gmail.com>
2021-02-02 22:55:35 +0900

    210202_DLstudy

--- DLpractice/memo3.py
+++ DLpractice/memo3.py
@@ -57,30 +57,58 @@ tf.disable_v2_behavior()
 # plt.show()
 
 
-x_data = [1,2,3]
-y_data = [1,2,3]
+# x_data = [1,2,3]
+# y_data = [1,2,3]
 
-W = tf.Variable(tf.random_normal([1]), name = 'weight')
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+# W = tf.Variable(tf.random_normal([1]), name = 'weight')
+# X = tf.placeholder(tf.float32)
+# Y = tf.placeholder(tf.float32)
 
+# hypothesis = X * W # Simplified
+# cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+
+# # # Gradient descent Algorithm
+# # 수동으로 직접 Minimize
+# # W -= Learinig rate * derivative
+
+# learning_rate = 0.1 # alpha 값
+# gradient = tf.reduce_mean((W * X - Y) * X) # 알파에 곱해진 수식 (평균)
+# descent = W - learning_rate * gradient
+# update = W.assign(descent) # 새로운 W값으로
+
+
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer()) # 변수 초기화
+
+# for step in range(21):
+#     sess.run(update, feed_dict = {X: x_data, Y: y_data})
+#     print(step, sess.run(cost, feed_dict = {X: x_data, Y: y_data}), sess.run(W))
+
+
+#### 210202 모두를 위한 딥러닝 강좌
+
+# 미분 수식이 복잡할 때 -> by 명령 optimize 선언
+# 미분하지 않아도 가능
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.1)
+# train = optimizer.minimize(cost)
+
+## W = 5일 때 optimize 시험해보자
+
+
+X = [1,2,3]
+Y = [1,2,3]
+
+W = tf.Variable(5.0)
 hypothesis = X * W # Simplified
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
-
-# # Gradient descent Algorithm
-# 수동으로 직접 Minimize
-# W -= Learinig rate * derivative
-
-learning_rate = 0.1 # alpha 값
-gradient = tf.reduce_mean((W * X - Y) * X) # 알파에 곱해진 수식 (평균)
-descent = W - learning_rate * gradient
-update = W.assign(descent) # 새로운 W값으로
-
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.1)
+train = optimizer.minimize(cost)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer()) # 변수 초기화
 
-for step in range(21):
-    sess.run(update, feed_dict = {X: x_data, Y: y_data})
-    print(step, sess.run(cost, feed_dict = {X: x_data, Y: y_data}), sess.run(W))
+for step in range(100):
+    print(step, sess.run(W))
+    sess.run(train)
---
+++ DLpractice/memo4.py
@@ -0,0 +1,2 @@
+#### 210203 모두를 위한 딥러닝 강좌 시즌
+## Lec 04 - multi-variable linear regression
