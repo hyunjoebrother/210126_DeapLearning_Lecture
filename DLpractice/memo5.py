6b9cbef71df381e23c2cbf3fd761aa54ccfbd6c7
hyunjoebrother <hyunjoe31@gmail.com>
2021-02-07 19:36:36 +0900

    210207_DLstudy

--- DLpractice/memo5.py
+++ DLpractice/memo5.py
@@ -1,5 +1,25 @@
 #### 210205 정통 21학번 신입생 정모로 pass
 
 #### 210206 모두를 위한 딥러닝 강좌 시즌
+
+#### 210207 모두를 위한 딥러닝 강좌 시즌
 ## Lec 05-1 - Logistic Classification의 가설 함수 정의
 
+# 복습 - Regression (결과값 y 예측하기)
+# Hypothesis -> Cost -> Gradient descent
+
+
+# ## (Binary) Classification
+# : 단순 숫자 예측이 아닌 2개 중 하나를 정해줌
+# ex. Spam Email Detection spam/ham, Facebook Feed show/hide
+# -> 0/1 encoding
+# ex. spam(1) or ham(0), show(1) or hide(0)
+
+# -> y값을 0~1 사이로 만들어주는 함수 g(z) : sigmoid function
+# : z=WX라 하면, 새로운 H(x) = g(z) 라고 가정하자
+
+# # Logistic Hypothesis
+# : Classification의 가설 함수가 된다
+
+
+## Lec 05-2 - Logistic Regression의 cost 함수 정의
